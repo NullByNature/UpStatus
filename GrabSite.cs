fc3ace1cbd42a6176829a58324854d678a5883da
NullByNature <austinplaylabs@gmail.com>
2025-08-19 16:45:54 -0400

    Add files via upload

---
+++ GrabSite.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GrabWebsite
+{
+    class WebsiteGrabber
+    {
+        public async void GetWebsite(string url)
+        {
+            HttpClient client = new HttpClient();
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Website is ONLINE");
+                }
+                else
+                {
+                    Console.WriteLine("Website is OFFLINE");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using GrabWebsite;
+
+namespace UpStatus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool runProgram = true;
+            while (runProgram)
+            {
+                // Hold off for 1 second so the message is not sent before returning website status 
+                Thread.Sleep(1000);
+
+                Console.WriteLine("Enter the full website link that you want to check");
+                string link = Console.ReadLine();
+                // make sure link is not blank 
+                while (string.IsNullOrEmpty(link))
+                {
+                    Console.WriteLine("Link can not be blank\n");
+                    link = Console.ReadLine();
+                }
+                WebsiteGrabber grabber = new WebsiteGrabber();
+                grabber.GetWebsite(link);
+            }
+        }
+    }
+}
